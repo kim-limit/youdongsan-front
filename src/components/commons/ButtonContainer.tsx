67a122761bfb0d6c41535798a93ce263e63da105
kim-limit <rlawogks2468@naver.com>
2022-12-30 18:20:05 +0900

    feat: 'add onclick event'

--- src/components/commons/ButtonContainer.tsx
+++ src/components/commons/ButtonContainer.tsx
@@ -1,14 +1,19 @@
-import { makeStyles } from "@mui/styles";
-import React from "react";
+import { makeStyles, Theme } from "@material-ui/core";
 
-const useStyles = makeStyles({
+import React, { useState } from "react";
+
+interface StyleProps {
+  selected: string;
+}
+const useStyles = makeStyles<Theme, StyleProps>((Theme) => ({
   btn: {
     color: "white",
     fontSize: "25px",
-    background: "#404040",
+    backgroundColor: "transparent",
     border: "0",
     width: "120px",
     height: "30px",
+
     "&:hover": {
       textDecoration: "underline",
       textDecorationColor: "#4EC5D6",
@@ -16,16 +21,80 @@ const useStyles = makeStyles({
       fontWeight: "bold",
     },
   },
-});
+  mainBtn: {
+    textDecoration: ({ selected }) =>
+      selected === "유동산" ? "underline" : "none",
+    textDecorationColor: ({ selected }) =>
+      selected === "유동산" ? "#4EC5D6" : "white",
+    textDecorationThickness: ({ selected }) =>
+      selected === "유동산" ? "8px" : "0px",
+    fontWeight: ({ selected }) => (selected === "유동산" ? "bold" : "bold"),
+  },
+  introduceBtn: {
+    textDecoration: ({ selected }) =>
+      selected === "회사 소개" ? "underline" : "none",
+    textDecorationColor: ({ selected }) =>
+      selected === "회사 소개" ? "#4EC5D6" : "white",
+    textDecorationThickness: ({ selected }) =>
+      selected === "회사 소개" ? "8px" : "0px",
+    fontWeight: ({ selected }) => (selected === "회사 소개" ? "bold" : "bold"),
+  },
+  noticeBtn: {
+    textDecoration: ({ selected }) =>
+      selected === "공지사항" ? "underline" : "none",
+    textDecorationColor: ({ selected }) =>
+      selected === "공지사항" ? "#4EC5D6" : "white",
+    textDecorationThickness: ({ selected }) =>
+      selected === "공지사항" ? "8px" : "0px",
+    fontWeight: ({ selected }) => (selected === "공지사항" ? "bold" : "bold"),
+  },
+  faqBtn: {
+    textDecoration: ({ selected }) =>
+      selected === "FAQ" ? "underline" : "none",
+    textDecorationColor: ({ selected }) =>
+      selected === "FAQ" ? "#4EC5D6" : "white",
+    textDecorationThickness: ({ selected }) =>
+      selected === "FAQ" ? "8px" : "0px",
+    fontWeight: ({ selected }) => (selected === "FAQ" ? "bold" : "bold"),
+  },
+}));
 
 export const ButtonContainer = () => {
-  const classes = useStyles();
+  const [selected, setSelected] = useState("유동산");
+  const classes = useStyles({
+    selected,
+  });
+
+  const buttonHandler = (e: React.MouseEvent<HTMLElement>) => {
+    setSelected(e.currentTarget.innerHTML);
+  };
+
   return (
     <div>
-      <button className={classes.btn}>{"유동산"}</button>
-      <button className={classes.btn}>{"회사 소개"}</button>
-      <button className={classes.btn}>{"공지사항"}</button>
-      <button className={classes.btn}>{"FAQ"}</button>
+      <button
+        className={`${classes.btn} ${classes.mainBtn}`}
+        onClick={buttonHandler}
+      >
+        {"유동산"}
+      </button>
+      <button
+        className={`${classes.btn} ${classes.introduceBtn}`}
+        onClick={buttonHandler}
+      >
+        {"회사 소개"}
+      </button>
+      <button
+        className={`${classes.btn} ${classes.noticeBtn}`}
+        onClick={buttonHandler}
+      >
+        {"공지사항"}
+      </button>
+      <button
+        className={`${classes.btn} ${classes.faqBtn}`}
+        onClick={buttonHandler}
+      >
+        {"FAQ"}
+      </button>
     </div>
   );
 };
