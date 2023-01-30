5e77190775e62872e954ab2b46a7bcdf4e806173
junni1111 <wkszlf98@naver.com>
2023-01-30 23:51:33 +0900

    ADD : notice, faq components

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { MainContainer } from "../components/commons/MainContainer";
 import { NavBar } from "../components/commons/NavBar";
+import {Faq} from "../components/faq/Faq";
 
 export const FaqPage = () => {
   return (
     <>
       {/* <NavBar path={"FAQ"} /> */}
-      <MainContainer></MainContainer>;
+      <MainContainer>
+          <Faq/>
+      </MainContainer>;
     </>
   );
 };
--- src/pages/NoticePage.tsx
+++ src/pages/NoticePage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { MainContainer } from "../components/commons/MainContainer";
-import { NavBar } from "../components/commons/NavBar";
+import {MainContainer} from "../components/commons/MainContainer";
+import {NavBar} from "../components/commons/NavBar";
+import {Notice} from "../components/notice/Notice";
 
 export const NoticePage = () => {
-  return (
-    <>
-      {/* <NavBar path={"공지사항"} /> */}
-      <MainContainer></MainContainer>;
-    </>
-  );
+    return (
+        <>
+            {/* <NavBar path={"공지사항"} /> */}
+            <MainContainer>
+                <Notice/>
+            </MainContainer>;
+        </>
+    );
 };
